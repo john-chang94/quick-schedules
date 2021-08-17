0359ed6625ffc46b3219465cecb16371ef180cdf
john-chang94 <j.chang94@yahoo.com>
2021-08-17 16:53:54 -0700

    update heroku build settings

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ app.use('/', require('./routes/storeRoutes'));
 //         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 //     })
 // }
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
