74db3d859c047a94f2c4e9d43cf7e38a69f79de0
john-chang94 <j.chang94@yahoo.com>
2021-08-17 19:45:24 -0700

    removed alternate production code

--- index.js
+++ index.js
@@ -25,11 +25,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('*', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
