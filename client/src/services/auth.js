7e5054075c2b8b34c7c7a4cc89c38b487f607a23
john-chang94 <j.chang94@yahoo.com>
2022-02-06 23:20:31 -0800

    update node connection

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const signIn = async (credentials) => {
     try {
-        const res = await axios.post('http://localhost:5000/auth/signin', credentials);
+        const res = await axios.post('/auth/signin', credentials);
         return res.data;
     } catch (err) {
         return { error: err.response.data };
