7e907e71970880ae5352441ac84b9718733a7bc0
john-chang94 <j.chang94@yahoo.com>
2022-04-21 15:52:55 -0700

    check dirname

--- index.js
+++ index.js
@@ -22,11 +22,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
   
   if (fs.existsSync(path.join(__dirname, "client/build"))) {
     console.log('exists');
+    console.log(path.join(__dirname, "client/build"))
   } else {
     console.log('does not exist')
   }
